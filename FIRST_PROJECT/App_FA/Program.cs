27b9d54cc957b8185e186124e9da5e3ca99b5858
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-06 15:34:48 +0300

    Add files via upload

---
+++ FIRST_PROJECT/App_FA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; // позволяет юзать имена стандартных классов из пространства имен system
+namespace ConsoleApplication1
+{
+    ///<summary>
+    ///Summary description for class 1
+    ///</summary>
+    class Class1
+    {
+        ///<summary>
+        ///The main entry point for the app
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //
+            // TODO Add code to start app here
+            //
+        }
+    }
+}
