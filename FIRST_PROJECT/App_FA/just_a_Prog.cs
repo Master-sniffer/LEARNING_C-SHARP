cf2a9cb77f4000270da16cbba31bfd89a7609ec5
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-06 16:01:36 +0300

    Create just_a_Prog.cs

---
+++ FIRST_PROJECT/App_FA/just_a_Prog.cs
@@ -0,0 +1,11 @@
+using System; 
+namespace ConsoleApplication1 
+{
+    class Class1 
+    {
+        static void Main()
+        {
+            Console.WriteLine("Слава Аллаху за этот прекрасный код");
+        }
+    }
+}
