142840e8be3303c84eb7714e6be85475d444488c
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-07 21:33:57 +0300

    Update Program.cs

--- FIRST_PROJECT/App_FA/Program.cs
+++ FIRST_PROJECT/App_FA/Program.cs
@@ -3,10 +3,17 @@ namespace ConsoleApplication1
 {
     class Class1
     {
-        int b = 5;
-        void Y()
+        static void Main()
         {
-            Console.WriteLine(b);
+            int x = 3, y = 3;
+            Console.Write("Значения префикса равно:");
+            Console.WriteLine(++x); // ВНИМАНИЕ НА МЕСТОПОЛОЖЕНИЕ X
+            Console.Write("Значение после приращения равно: ");
+            Console.WriteLine(x);
+            Console.Write("Значения постфикса равно:");
+            Console.WriteLine(y++); // ВНИМАНИЕ НА МЕСТОПОЛОЖЕНИЕ Y
+            Console.Write("Значение после приращения равно: ");
+            Console.WriteLine(y);
         }
     }
 }
