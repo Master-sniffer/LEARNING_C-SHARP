7fc03c87243231f09eca74b0c84af26255720c1c
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-08 22:41:03 +0300

    Add files via upload

--- FIRST_PROJECT/App_FA/Program.cs
+++ FIRST_PROJECT/App_FA/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.IO;
 namespace ConsoleApplication1
 {
     class Class1
     {
         static void Main()
-        {
-            int x = 3, y = 3;
-            Console.Write("Значения префикса равно:");
-            Console.WriteLine(++x); // ВНИМАНИЕ НА МЕСТОПОЛОЖЕНИЕ X
-            Console.Write("Значение после приращения равно: ");
-            Console.WriteLine(x);
-            Console.Write("Значения постфикса равно:");
-            Console.WriteLine(y++); // ВНИМАНИЕ НА МЕСТОПОЛОЖЕНИЕ Y
-            Console.Write("Значение после приращения равно: ");
-            Console.WriteLine(y);
-        }
+        { // МЕТОД КЛАССА MATH
+            Console.WriteLine("Enter x: ");
+            double x = Convert.ToDouble(Console.ReadLine()); // ТУТ СРАЗУ ИДЕТ ПРИСВАИВАНИЕ ( БУДЕТ ПОЛЕЗНО НА БУДУЩЕЕ)
+
+            Console.WriteLine("Enter Alfa");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double y = Math.Sqrt(Math.PI * x) -
+                Math.Exp(0.2 * Math.Sqrt(a)) +
+                2 * Math.Tan(2 * a) +
+                1.6e3 * Math.Log10(Math.Pow(x, 2));
+
+            Console.WriteLine("For x={0} and alfa = {1}", x, a);
+            Console.WriteLine("Result is = " + y);
+        } 
     }
 }
