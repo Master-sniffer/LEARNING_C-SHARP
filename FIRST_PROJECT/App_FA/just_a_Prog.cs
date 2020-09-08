ce1179ee5675ce7ccb2cd334e7f5e01987e3ea0b
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-08 22:38:54 +0300

    Update just_a_Prog.cs

--- FIRST_PROJECT/App_FA/just_a_Prog.cs
+++ FIRST_PROJECT/App_FA/just_a_Prog.cs
@@ -1,4 +1,5 @@
 using System; // позволяет юзать имена стандартных классов из пространства имен system
+using System.IO; // Для работы с файлами и не только !
 namespace ConsoleApplication1 // создает для проекта собственное пр-во имен , названное по умолчанию ConsoleApplication 1
 {
     ///<summary>
@@ -278,4 +279,30 @@ namespace ConsoleApplication1 // создает для проекта собст
 
 
 
-e
+    class Class1
+    {
+        static void Main()
+        { // МЕТОД КЛАССА MATH
+            Console.WriteLine("Enter x: ");
+            double x = Convert.ToDouble(Console.ReadLine()); // ТУТ СРАЗУ ИДЕТ ПРИСВАИВАНИЕ ( БУДЕТ ПОЛЕЗНО НА БУДУЩЕЕ)
+
+            Console.WriteLine("Enter Alfa");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double y = Math.Sqrt(Math.PI * x) -
+                Math.Exp(0.2 * Math.Sqrt(a)) +
+                2 * Math.Tan(2 * a) +
+                1.6e3 * Math.Log10(Math.Pow(x, 2));
+
+            Console.WriteLine("For x={0} and alfa = {1}", x, a);
+            Console.WriteLine("Result is = " + y);
+        } 
+    }
+
+
+
+##
+
+
+
+d
