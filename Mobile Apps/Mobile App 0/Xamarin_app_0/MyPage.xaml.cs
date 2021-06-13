fadcd0b3f03b8620f530f43587f6f5ec07b5b867
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2021-06-14 02:16:39 +0300

    Add files via upload

---
+++ Mobile Apps/Mobile App 0/Xamarin_app_0/MyPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin_app_0
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+
+        void Slider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Value is {0:F2}", e.NewValue); // Выводим ток 2 числа после запятой
+
+        }
+
+    }
+}
