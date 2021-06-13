e2cf32e37e8884df17bc08d5e7d83d2d08c2c1b6
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2021-06-14 02:30:18 +0300

    Add files via upload

---
+++ Mobile Apps/Mobile App 0/Xamarin_app_0/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_app_0
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MyPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
